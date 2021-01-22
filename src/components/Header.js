ef2ee5120f0b11385fbebb7bc9fa208bdeadefd9
Jurgen Clausen <jurgen1c@gmail.com>
2021-01-22 16:56:47 -0600

    Add header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,17 +1,30 @@
-
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
 const Header = () => {
     return (
         <div className='header'>
             <img className='header__logo' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
             <div className='header__search'>
                 <input className='header__searchInput' type='text'/>
+                <SearchIcon className='header__searchIcon'/>
             </div>
             <div className='headerNav'>
                 <div className='header__option'>
                     <span className='header__optionLineOne'>Hello Guest</span>
+                    <span className='header__optionLineTwo'> Sign In</span>
                 </div>
-                <div className='header__option'></div>
-                <div className='header__option'></div>
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Returns</span>
+                    <span className='header__optionLineTwo'>& Orders</span>
+                </div>
+                <div className='header__option'>
+                    <span className='header__optionLineOne'>Your</span>
+                    <span className='header__optionLineTwo'> Prime</span>
+                </div>
+                <div className='header__optionBasket'>
+                    <ShoppingBasketIcon />
+                    <span className='header__optionLineTwo header__basketCount'>0</span>
+                </div>
             </div>
         </div>
     )
